281a52d8540b206c515bc91bc36f6038309e0230
Winne-inne <abjeanbaptiste@davidson.edu>
2024-09-03 23:19:32 -0400

    Finished Hello World

---
+++ LearnJava.java
@@ -0,0 +1,21 @@
+public class LearnJava {
+    public static void main(String[] args) {
+        
+        String name = printName("Winne", 8);
+    }
+
+    public static String printName(String name, int number) {
+       
+        if (){
+
+        }
+        else if(){
+
+        }
+        else {
+            
+        }
+    }
+
+
+}
